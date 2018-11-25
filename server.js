5aa60b1a5d590c663b95800aabdadfc07fda6bf0
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-11-25 15:42:01 -0700

    Update to start of project

--- server.js
+++ server.js
@@ -7,46 +7,15 @@ const port = 3000;
 
 app.use(cors());
 
+// first endpoint - already built
+// this says that when the app is at "/" - so "home", it will send a response of "Hedgehog Time". That is why we see those words on the screen when we go to localhost:3000/.
 app.get('/', (req, res) => {
   res.send("Hedgehog Time");
 });
 
-app.get('/hedgie/:keyword', (req, res) => {
-  var keyword = req.params.keyword
+// your endpoint
 
-  function findHedgieImage(keyword) {
-    var nightmare = Nightmare();
-
-    return nightmare
-      .goto('https://www.google.com')
-      .insert('input[title="Search"]', `hedgehog ${keyword}`)
-      .click('input[value="Google Search"]')
-      .wait('a.q.qs')
-      .click('a.q.qs')
-      .wait('div#res.med')
-      .evaluate(function() {
-        var photoDivs = document.querySelectorAll('img.rg_ic');
-        var list = [].slice.call(photoDivs);
-
-        return list.map(function(div) {
-          return div.src;
-        });
-      })
-      .end()
-      .then(function (result) {
-        return result.slice(1, 5);
-      })
-      .then(function (images) {
-        res.json(images)
-      })
-      .catch(function (error) {
-        console.error('Search failed:', error);
-      });
-  }
-
-  findHedgieImage(keyword);
-
-});
+// scraper endpoint
 
 app.listen(port, () => {
   console.log(`app running on ${port}`);
