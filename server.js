e0825b13b5bd45a4b99665d6b4a84c449895fca2
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-12-02 16:14:30 -0700

    everything is great

--- server.js
+++ server.js
@@ -15,36 +15,39 @@ app.get('/', (req, res) => {
 // scraper endpoint
 app.get('/history/:keyword', (req,res) => {
   var keyword= req.params.keyword;
+
   function findHistoryImage(keyword) {
-    var nightmare= Nightmare ({ show: true});
+    var nightmare = Nightmare({ show: true});
+
     return nightmare
        .goto('https://www.google.com')
-       .insert('input [title= "Search"]', `history ${keyword}`)
-       .click('input [value="Google Search"]')
+       .insert('input[title= "Search"]', `history ${keyword}`)
+       .click('input[value="Google Search"]')
        .wait('a.q.qs')
        .click('a.q.qs')
        .wait('div#res.med')
        .evaluate(function() {
-         var photoDivs= document.querySelectorAll ('img.rg_ic')
+         var photoDivs= document.querySelectorAll('img.rg_ic');
          var list= [].slice.call(photoDivs);
-         return list.map(function (div){
+
+         return list.map(function(div) {
            return div.src;
          });
        })
        .end()
-       .then(function(result){
+       .then(function(result) {
          return result.slice(1,5);
        })
        .then(function(images){
          res.json(images);
        })
        .catch (function(error) {
-         console.error('Search failed', error);
-       })
+         console.error('Search failed:', error);
+       });
   }
-  findHistoryImage (keyword);
-  var nightmare = Nightmare({show:true});
+  findHistoryImage(keyword);
 });
+
 app.listen(port, () => {
   console.log(`app running on ${port}`);
 });
