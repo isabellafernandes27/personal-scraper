b9b7aa9a7091910b1a1ca2a1836a7eedbe42f027
Amy Holt <amy.elizabeth.holt@gmail.com>
2018-12-02 15:51:56 -0700

    added server.js

--- server.js
+++ server.js
@@ -9,12 +9,42 @@ app.use(cors());
 
 // first endpoint - already built
 app.get('/', (req, res) => {
-  res.send("This is my personal web scraper!");
+  res.send("History through Images");
 });
 
 // scraper endpoint
-
-
+app.get('/history/:keyword', (req,res) => {
+  var keyword= req.params.keyword;
+  function findHistoryImage(keyword) {
+    var nightmare= Nightmare ({ show: true});
+    return nightmare
+       .goto('https://www.google.com')
+       .insert('input [title= "Search"]', `history ${keyword}`)
+       .click('input [value="Google Search"]')
+       .wait('a.q.qs')
+       .click('a.q.qs')
+       .wait('div#res.med')
+       .evaluate(function() {
+         var photoDivs= document.querySelectorAll ('img.rg_ic')
+         var list= [].slice.call(photoDivs);
+         return list.map(function (div){
+           return div.src;
+         });
+       })
+       .end()
+       .then(function(result){
+         return result.slice(1,5);
+       })
+       .then(function(images){
+         res.json(images);
+       })
+       .catch (function(error) {
+         console.error('Search failed', error);
+       })
+  }
+  findHistoryImage (keyword);
+  var nightmare = Nightmare({show:true});
+});
 app.listen(port, () => {
   console.log(`app running on ${port}`);
 });
